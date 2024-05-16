63224ec131beb4d7d105deca128804bf976cf06e
Savteen <den766sav@gmail.com>
2024-05-16 14:42:27 +0300

    Task 2.2

--- HW8.2/HW8.2.cpp
+++ HW8.2/HW8.2.cpp
@@ -13,12 +13,25 @@ int factorial(int count)
 	return F;
 }
 
+int factorialRecursion(int count)
+{
+	if (count == 0 || count == 1)
+	{
+		return 1;
+	}
+	return count * factorialRecursion(count - 1);
+}
+
 int main()
 {
+	std::cout << "Enter your factorial number \n";
 	int F;
-	std::cout << "Enter your number! : ";
+	std::cout << "FIRST way to calculate Factorial \n";
 	std::cin >> F;
 	factorial(F);
-
+	std::cout << "\n";
+	std::cout << "SECOND way to calculate Factorial:" << std::endl;
+	int recurFactorial = factorialRecursion(F); 
+	std::cout << "Result: " << recurFactorial << std::endl;
 }
 
