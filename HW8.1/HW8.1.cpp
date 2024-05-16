2c7c6777b86389fd61bc600911b9421d9180ad0d
Savteen <den766sav@gmail.com>
2024-05-16 12:24:58 +0300

    Task 1

---
+++ HW8.1/HW8.1.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+
+void function(int ammountOfNumbers)
+{
+	while (true)
+	{
+		int number=0,positive=0,negative=0,zero=0;
+
+		for (int i=0 ;i < ammountOfNumbers; i++)
+		{
+			std::cout << "Input your number : ";
+			std::cin >> number;
+			if (number > 0)
+			{
+				positive++;
+			}
+			else if (number < 0)
+			{
+				negative++;
+			}
+			else
+			{
+				zero++;
+			}
+		}
+		std::cout << "positive : " << positive <<"\n";
+		std::cout << "negative : " << negative << "\n";
+		std::cout << "zero : " << zero << "\n";
+	}
+	return 0;
+}
+
+int main()
+{
+	int rounds=0;
+	std::cout << "Enter ammount of numbers: ";
+	std::cin >> rounds;
+	function(rounds);
+}
+
+
