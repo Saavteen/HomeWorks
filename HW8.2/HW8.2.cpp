2e5e6a311b69502906ff5558ed40641bc0e284ab
Savteen <den766sav@gmail.com>
2024-05-16 12:53:01 +0300

    Task 2

---
+++ HW8.2/HW8.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int factorial(int count)
+{
+	int F = 1;
+
+	for ( int a = 1; a <= count; ++a)
+	{
+		F *= a;
+		std::cout << F << " ";
+
+	}
+	return F;
+}
+
+int main()
+{
+	int F;
+	std::cout << "Enter your number! : ";
+	std::cin >> F;
+	factorial(F);
+
+}
+
