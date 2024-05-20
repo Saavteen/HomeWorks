93ba99cbafbda7398317ebf069b8711d8abcb67d
Savteen <den766sav@gmail.com>
2024-05-21 00:38:45 +0300

    task 2

---
+++ HW10.2/HW10.2.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+bool calculateSum(const double* arr, int arrSize, double& sum)
+{
+
+    int a=0;
+    for (int i = 0; i < arrSize; i++)
+    {
+        sum += arr[i];
+    }
+    if (sum > 0)
+    {
+        std::cout << sum;
+        return true;
+    }
+    else
+    {
+        std::cout << "eror less than 0";
+        return false;
+    }
+}
+
+int main()
+{
+    double sum = 0;
+    const int lineSize = 5;
+    double line[lineSize];
+
+    std::cout << "string: ";
+    for (int i = 0; i < lineSize; i++) 
+    {
+        std::cin >> line[i];  
+    }
+    calculateSum(line,lineSize,sum);
+
+}
