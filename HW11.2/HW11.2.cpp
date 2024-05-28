869f521e32803b40eafa2ca6c400785c9827257e
Savteen <den766sav@gmail.com>
2024-05-28 15:48:51 +0300

    task 2

---
+++ HW11.2/HW11.2.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+enum class SortingDirection 
+{ 
+  ascending, 
+  descending 
+};
+
+bool isSorted(const int* arr, int size, SortingDirection direction)
+{
+    for (int i=0;i<size;i++)
+    {
+        if (direction == SortingDirection::ascending)
+        {
+            if (arr[i - 1] > arr[i])
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (arr[i - 1] < arr[i])
+            {
+                return false;
+            }
+        }
+    }   
+    return true;
+}
+
+
+
+int main()
+{
+    const int maxSize=6;
+    int arr[maxSize];
+    for (int i = 0; i < maxSize; i++)
+    {
+        std::cin >> arr[i];
+    }
+    std::cout << "is sorted in ascending order : " << (isSorted(arr, maxSize, SortingDirection::ascending) ? "true" : "false\n Its sorted in descending order ") << std::endl;
+}
+
