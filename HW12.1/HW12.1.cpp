65f0c43a44f9e534ed3d9291ea13295740b7c5f3
Savteen <den766sav@gmail.com>
2024-06-01 00:02:28 +0300

    task 1

---
+++ HW12.1/HW12.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+void bubbleSort(int arr[], int size) 
+{
+    for (int i = 0; i < size - 1; i++) 
+    {
+        for (int j = 0; j < size - i - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() 
+{
+    int arr[] = {3, 5, 25, 12, 22, 3, 90 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: \n";
+    for (int i = 0; i < size; i++) 
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    bubbleSort(arr, size);
+
+    std::cout << "Sorted array: \n";
+    for (int i = 0; i < size; i++) 
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
