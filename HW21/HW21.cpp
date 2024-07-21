8fba679b7ac4c8d49fcd221595a744dc7a261dde
Savteen <den766sav@gmail.com>
2024-07-21 13:37:47 +0300

    task 1 A

---
+++ HW21/HW21.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+bool compareByLength(const std::string& a, const std::string& b) {
+    return a.size() < b.size();
+}
+
+int main() {
+    std::vector<std::string> stringContainer = { "apple", "banana", "pear", "grapefruit", "kiwi" };
+
+    std::sort(stringContainer.begin(), stringContainer.end(), compareByLength);
+
+    for (const auto& str : stringContainer) {
+        std::cout << str << " ";
+    }
+
+    return 0;
+}
