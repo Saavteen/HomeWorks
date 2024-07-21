046e3b97a213e5d6b7628bbc43a734a6a9f67b40
Savteen <den766sav@gmail.com>
2024-07-21 13:44:08 +0300

    task 2

---
+++ HW21.4/HW21.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+unsigned int countDivisibleBy(const std::vector<int>& vec, int number) 
+{
+    return std::count_if(vec.begin(), vec.end(), [number](int x) 
+        {
+        return x % number == 0;
+        });
+}
+
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,12 };
+    
+    int number = 2;
+    unsigned int count = countDivisibleBy(numbers, number);
+
+    std::cout << "Number of elements divisible by " << number << " is " << count << std::endl;
+
+    return 0;
+}
