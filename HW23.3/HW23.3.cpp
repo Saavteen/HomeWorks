f55917ca272394c682250d4cf193b746d21bc494
Savteen <den766sav@gmail.com>
2024-08-01 13:23:59 +0300

    Task 3

---
+++ HW23.3/HW23.3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <stack>
+#include <unordered_map>
+
+bool areBracketsBalanced(const std::string& line)
+{
+    std::stack<char> stack;
+    std::unordered_map<char, char> matchingBrackets = 
+    {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
+    };
+
+    for (char ch : line) {
+        if (ch == '(' || ch == '{' || ch == '[') 
+        {
+            stack.push(ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']') 
+        {
+            if (stack.empty() || stack.top() != matchingBrackets[ch])
+            {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.empty();
+}
+
+int main() {
+    std::string line = "{ [() ] }()";
+    if (areBracketsBalanced(line)) 
+    {
+        std::cout << "OK" << std::endl;
+    }
+    else 
+    {
+        std::cout << "NOT OK" << std::endl;
+    }
+
+    line = "{ [ ( ) ] }";
+    if (areBracketsBalanced(line)) 
+    {
+        std::cout << "OK" << std::endl;
+    }
+    else 
+    {
+        std::cout << "NOT OK" << std::endl;
+    }
+
+    return 0;
+}
