e9583d79391bdd3613b35f5d3f24f0b1f58c105d
Savteen <den766sav@gmail.com>
2024-08-01 13:20:18 +0300

    Task 1

---
+++ HW23.1/HW23.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <sstream>
+
+unsigned int uniqueWordsCount(const std::string& line) {
+    std::unordered_set<std::string> uniqueWords;
+    std::stringstream ss(line);
+    std::string word;
+
+    while (ss >> word) {
+        uniqueWords.insert(word);
+    }
+
+    return uniqueWords.size();
+}
+
+int main() {
+    std::string line = "Hello Hey Hello Bye Hey";
+    std::cout << "Unique words count: " << uniqueWordsCount(line) << std::endl;
+    return 0;
+}
